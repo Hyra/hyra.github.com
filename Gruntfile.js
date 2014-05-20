9b4c2ed4fac23d3b548fb44ba9b44383df7c497e
Hyra <stef@mindthecode.com>
2014-05-20 07:32:33 +0200

    Added demos folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,6 +55,7 @@ module.exports = function (grunt) {
             'scripts/**',
             'styles/**.css',
             'styles/fonts/**',
+            'demos/**',
           ]
         }]
       }
